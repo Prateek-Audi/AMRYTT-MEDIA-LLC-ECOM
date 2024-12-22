5bcb1eadb9bcf0e249451a01bfdd4c1578fe88a8
Prateek Audichya <prateek.audi.pa@gmail.com>
2024-12-22 16:03:44 +0530

    h-full

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -3,7 +3,7 @@ import DashboardPage from "@/components/Dashboard";
 
 const Page: NextPage = () => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-[100%]">
       <DashboardPage />
     </div>
   );
