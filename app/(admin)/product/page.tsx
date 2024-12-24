b0e57fee2ff01b2dca7235c86a7d312438c9eb95
Prateek Audichya <prateek.audi.pa@gmail.com>
2024-12-24 13:32:03 +0530

    datatable added

--- app/(admin)/product/page.tsx
+++ app/(admin)/product/page.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from "next";
+import ProductTable from "@/components/Product";
 
 const Page: NextPage = () => {
-  return <>Product List</>;
+  return (
+    <main className="px-4">
+      <ProductTable />
+    </main>
+  );
 };
 
 export default Page;
